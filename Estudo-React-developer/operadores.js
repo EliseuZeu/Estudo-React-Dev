404c5b6133275afb7d1a5c658b18f9c717154040
zeusouza <eliseu.souza@mail.usf.edu.br>
2023-03-08 21:39:04 -0300

    Operadores aritmeticos javascript

---
+++ Estudo-React-developer/operadores.js
@@ -0,0 +1,8 @@
+const valor1 = 10; /*VARIAVEL*/ 
+const valor2 = 2; /* VARIAVEL */
+
+console.log('Soma: ',valor1 + valor2);
+console.log('Subtração: ',valor1 - valor2);
+console.log('Multiplicação: ',valor1 * valor2);
+console.log('Divisao: ',valor1 / valor2);
+console.log('Resto: ',valor1 % valor2);
